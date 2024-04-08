84a9927cfdf3907d82749501273c1aa0d6d26626
SobczykMichal <sobczykmichal01@gmail.com>
2024-04-08 18:32:04 +0200

    FINISH WHOLE PROJECT, KMEANS, RANSAC

--- lab2_algorytmy.py
+++ lab2_algorytmy.py
@@ -4,25 +4,9 @@ from sklearn.linear_model import RANSACRegressor
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 
-def norm_vec(X, y, reg_ransac):
-    reg_ransac.fit(X, y)
-    normal_vector = reg_ransac.estimator_.coef_
-    return normal_vector
-
-def is_plane(X, y, normal_vector, threshold=0.1):
-    distances = np.abs(y - (X @ normal_vector))
-    avg = np.mean(distances)
-    if avg < threshold:
-        return True
-    else:
-        return False
-
-def is_vertical(normal_vector):
-    vertical = np.isclose(normal_vector, [0, 0, 1]).all() or np.isclose(normal_vector, [0, 0, -1]).all()
-    return vertical
-
 
 def load_data_from_file():
+    #load data
     with open('dane_cwiczenie2.xyz', 'r') as file:
         reader = csv.reader(file)
         for row in reader:
@@ -30,33 +14,82 @@ def load_data_from_file():
             yield x, y, z
 
 
-if __name__ == '__main__':
-    chmury = list(load_data_from_file())
-    alg_kmeans = KMeans(n_clusters=3)
-    cluster = alg_kmeans.fit_predict(chmury)
+def Kmeans_method(points, n_clusters=3):
+    #Kmeans method, set clusters, show method
+    alg_kmeans = KMeans(n_clusters=n_clusters)
+    cluster = alg_kmeans.fit_predict(points)
     chart = plt.figure()
+    plt.title('KMeans method')
     chart3D = chart.add_subplot(111, projection='3d')
     colors = ['r', 'g', 'b']
+    for i in range(n_clusters):
+        matrix_points = np.array(points)[cluster == i]
+        chart3D.scatter(matrix_points[:, 0], matrix_points[:, 1], matrix_points[:, 2], c=colors[i], label=f'Przyporzadkowanie {i + 1}')
+        plane_other, inlier_mask_other = fit_plane_ransac_sklearn(matrix_points)
 
-    for i in range(3):
-        tablica = np.array(chmury)[cluster == i]
-        Y = tablica
-        ransac = RANSACRegressor()
-        normal_vector = norm_vec(tablica, Y, ransac)
-        check_plane = is_plane(tablica, Y, normal_vector)
-        check_orient = is_vertical(normal_vector)
-        print("Wyniki dla chmury ", i+1, ':')
-        print("Wektor normalny:", normal_vector)
-        print("Czy chmura jest płaszczyzną:")
-        if check_plane:
-            print("Tak")
-            print("Czy płaszczyzna jest pionowa:")
-            if check_orient:
-                print("Tak")
-            else:
-                print("Nie")
+        print("Wektor normalny do płaszczyzny ", i+1, ":", plane_other[:3])
+        avg = avg_distance(plane_other, matrix_points)
+        print("Średnia odległość punktów od płaszczyzny:", avg)
+        if (np.linalg.norm(plane_other[:3]) < 5):
+            print("Chmura tworzy plaszczyzne")
         else:
-            print("Nie")
-        chart3D.scatter(tablica[:, 0], tablica[:, 1], tablica[:, 2], c=colors[i], label=f'Przyporzadkowanie {i + 1}')
+            print("Chmura nie tworzy plaszczyzny")
+        normalized_vector = plane_other[:3]/np.linalg.norm(plane_other[:3])
+        if np.abs(normalized_vector[0]) < 0.1:
+            if np.abs(normalized_vector[1]) < 0.1:
+                print("Płaszczyzna jest pozioma")
+            else:
+                print("Płaszczyzna jest pionowa")
+        else:
+            print("Płaszczyzna o innej orientacji")
+
+
+def fit_plane_ransac_sklearn(points, max_trials=100):
+     X = points[:, :2]  # Współrzędne x i y
+     y = points[:, 2]  # Współrzędna z
+        # RANSAC method
+     ransac = RANSACRegressor(max_trials=max_trials)
+     ransac.fit(X, y)
+
+     # plane
+     a, b = ransac.estimator_.coef_
+     c = ransac.estimator_.intercept_
+
+     return np.array([a, b, -1, c]), ransac.inlier_mask_  # matrix of plane [a, b, -1, c] for ax+by-z+c=0
+
+
+def plot_RANSAC(plane, points, inlier_mask):
+    a, b, _, c = plane
+
+    inliers = points[inlier_mask]
+    outliers = points[~inlier_mask]
+
+    chart = plt.figure()
+    plt.title('RANSAC method')
+    chart3D = chart.add_subplot(111, projection='3d')
+
+    chart3D.scatter(inliers[:, 0], inliers[:, 1], inliers[:, 2], c='b', label='Inliers')
+    chart3D.scatter(outliers[:, 0], outliers[:, 1], outliers[:, 2], c='r', label='Outliers')
+
+
+def avg_distance(plane, points):
+    a, b, _, c = plane
+    distances = np.abs(a * points[:, 0] + b * points[:, 1] - points[:, 2] + c) / np.sqrt(a**2 + b**2 + (-1)**2)
+    return np.mean(distances)
+
+
+if __name__ == '__main__':
+
+    cloud_points = list(load_data_from_file())
+    tablica = np.array(cloud_points)
+    plane_from_clouds, inlier_Points = fit_plane_ransac_sklearn(tablica)
+    plot_RANSAC(plane_from_clouds, tablica, inlier_Points)
+
+    print("Wektor normalny do calkowitej płaszczyzny:", plane_from_clouds[:3])
+    mean_distance = avg_distance(plane_from_clouds, tablica)
+    print("Średnia odległość punktów od płaszczyzny:", mean_distance)
+
+    Kmeans_method(cloud_points, 3)
+
     plt.legend()
     plt.show()
